012fd2ca789576ac07850bf92be5dfcad5482978
Wasu Wasusatein <megaauuuuu@gmail.com>
2020-04-30 16:48:43 +0900

    my-import-test.py and some vscode thingy?

---
+++ pointnet2/my-import-test.py
@@ -0,0 +1,19 @@
+import argparse
+import math
+from datetime import datetime
+import h5py
+import numpy as np
+import tensorflow as tf
+import socket
+import importlib
+import os
+import sys
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = BASE_DIR
+sys.path.append(BASE_DIR)
+sys.path.append(os.path.join(ROOT_DIR, 'models'))
+sys.path.append(os.path.join(ROOT_DIR, 'utils'))
+import provider
+import tf_util
+
+print('ok')
